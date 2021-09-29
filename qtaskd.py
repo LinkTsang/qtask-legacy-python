8305b20bea5682518794cf824bc02b698a963f6e
Link <linkdoyle@163.com>
2021-09-29 20:25:49 +0800

    func: catch and log any exceptions in the event loop.

--- qtaskd.py
+++ qtaskd.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from typing import List
 
 from TaskScheduler import TaskScheduler
@@ -7,32 +8,37 @@ from utils import setup_logger
 
 
 async def main(scheduler: TaskScheduler):
-    demo_tasks: List[TaskInfo] = [
-        TaskInfo(
-            name="task 1",
-            working_dir=".",
-            command_line="python -m demo.task1",
-            output_file_path="task1.output.log"
-        ),
-        TaskInfo(
-            name="task 2",
-            working_dir=".",
-            command_line="python -m demo.task2",
-            output_file_path="task2.output.log"
-        ),
-    ]
+    logger = logging.getLogger('qtaskd')
+    logger.info('running task scheduler...')
 
-    print('running task scheduler...')
+    try:
+        demo_tasks: List[TaskInfo] = [
+            TaskInfo(
+                name="task 1",
+                working_dir=".",
+                command_line="python -m demo.task1",
+                output_file_path="task1.output.log"
+            ),
+            TaskInfo(
+                name="task 2",
+                working_dir=".",
+                command_line="python -m demo.task2",
+                output_file_path="task2.output.log"
+            ),
+        ]
 
-    scheduler.init_asyncio()
-    scheduler_task = asyncio.create_task(scheduler.run())
+        scheduler.init_asyncio()
+        scheduler_task = asyncio.create_task(scheduler.run())
 
-    for t in demo_tasks:
-        scheduler.add_task(t)
+        for t in demo_tasks:
+            scheduler.add_task(t)
 
-    await scheduler_task
+        await scheduler_task
 
-    print('task scheduler exited.')
+    except Exception:
+        logger.exception('task scheduler uncaught exception')
+
+    logger.info('task scheduler exited.')
 
 
 if __name__ == "__main__":
